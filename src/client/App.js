62f451f1f217ed3ec9b73dadc94bd2aefe4ff85a
idmitrov <mailto.dimitrov@gmail.com>
2017-07-09 14:07:12 +0300

    Clean client

--- src/client/App.js
+++ src/client/App.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 
-import HomePage from './components/Home'
-import TestComponent from './components/TestComponent'
-import TestComponent2 from './components/TestComponent2'
-import PageNotFound from './components/PageNotFound'
+import HomePage from './components/home/HomePage'
+import NotFoundPage from './components/shared/NotFoundPage'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Switch>
-            <Route exact path ='/' component={ HomePage } />
-            <Route exact path ='/test' component={ TestComponent }/>
-            <Route exact path ='/test/:id' component={ TestComponent2 } />
-            <Route path='*' component={ PageNotFound } />
-          </Switch>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app-wrapper">
+                <Switch>
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='*' component={NotFoundPage} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
