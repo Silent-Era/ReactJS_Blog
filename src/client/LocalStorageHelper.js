b7d35f002780e48c11faeefc65cb8a27b26b1b1d
Silent-Era <silentera4@gmail.com>
2017-07-10 23:53:20 +0300

    Helper for local storage in client side

---
+++ src/client/LocalStorageHelper.js
@@ -0,0 +1,17 @@
+class LocalStorageHelper{
+    static authenticateUser (token) {
+    window.localStorage.setItem('token', token)
+  }
+  static isUserAuthenticated () {
+    return window.localStorage.getItem('token') !== null
+  }
+  static deauthenticateUser () {
+    window.localStorage.removeItem('token')
+  }
+  static getToken () {
+    return window.localStorage.getItem('token')
+  }
+
+}
+
+export default LocalStorageHelper
---
+++ src/server/middlewares/auth-check.js
@@ -0,0 +1,40 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const secretStr = 'some random bullshit333'
+
+module.exports = (req, res, next) => {
+    let authHeaders = req.headers.authorization
+    if(!authHeaders){
+        return res.status(401).send('Invalid auth headers')
+    }
+
+    let token = authHeaders.split(' ')[1]
+    if(!token){
+        return res.status(401).send('Invalid token')
+    }
+
+    jwt.verify(token, secretStr, (err, decoded) => {
+        console.log(token)
+        if(err) return res.status(401).send('Error while handling the token')
+
+        let userId = decoded.sub, 
+            password = decoded.password
+        if(!userId || !password) return res.status(401).send('Invalid token')
+
+        User.findById(userId).then(foundUser => {
+            if(!foundUser || foundUser.password !== password){
+                if(!foundUser)
+                    return res.status(401).send('In user problem')
+                res.status(404).json({
+                    reqPass:password,
+                    foundPass:foundUser.password
+                })
+            }
+
+            req.user = foundUser
+
+            next()
+        })
+    })
+
+}
